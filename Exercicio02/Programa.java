ff28f2671b5fe80f91894883ecc3c866f9292556
Edu Machado <eduardomachadoh1@gmail.com>
2024-03-20 17:02:09 -0300

    Exercicio 02

--- Exercicio02/Programa.java
+++ Exercicio02/Programa.java
@@ -1,6 +1,5 @@
 package Exercicio02;
 import Exercicio02.classes.Automovel;
-import Exercicio02.classes.Imprimir;
 import Exercicio02.Prompt;
 public class Programa {
    public static void main(String[] args) {
