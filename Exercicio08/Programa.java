f2d276e68e2f9fa92079f72da0e2d11cf6d307a3
Edu Machado <eduardomachadoh1@gmail.com>
2024-04-15 16:09:38 -0300

    Exercicio 08

--- Exercicio08/Programa.java
+++ Exercicio08/Programa.java
@@ -2,17 +2,17 @@ package Exercicio08;
 import Exercicio08.classes.Aluno;
 public class Programa {
     public static void executar() {
-        
-        String nome = Prompt.lerLinha("Nome do aluno: ");
-        double[] notas = new double[3];
-        for (int i = 0; i < 3; i ++) {
-            notas[i] = Prompt.lerDecimal("Nota:");
-        }
        
-        Aluno aluno1 = new Aluno(nome, notas[0], notas[1], notas[2]);
-        aluno1.calculoMedia();
+        Aluno aluno1 = new Aluno("Joao", 4, 3, 9);
+        aluno1.setNome("Pedro");
+        aluno1.setNota1(10);
+        aluno1.setNota2(5);
+        aluno1.setNota3(10);
 
-        System.out.printf("Nome: %s\n", aluno1.nome);
-        System.out.printf("Status: %s", aluno1.status);
+
+
+        System.out.printf("Media: %.1f\n", aluno1.calcularMedia());
+        System.out.printf("Nome: %s\n", aluno1.getNome());
+        System.out.printf("Status: %s", aluno1.calcularStatus());
     }
 }
--- Exercicio08/classes/Aluno.java
+++ Exercicio08/classes/Aluno.java
@@ -1,23 +1,73 @@
 package Exercicio08.classes;
 
 public class Aluno {
-    public double nota1;
-    public double nota2;
-    public double nota3;
-    public String nome;
-    public double media;
-    public String status;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    private String nome;
+    private double media;
+    private String status;
 
     public Aluno( String nome, double nota1, double nota2, double nota3) {
         this.nota1 = nota1;
         this.nota2 = nota2;
         this.nota3 = nota3;
         this.nome = nome;
-
-        this.media = (this.nota1 + this.nota2 + this.nota3) / 3;
     }
 
-    public void calculoMedia() {
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double calcularMedia() {
+        return this.media = (this.nota1 + this.nota2 + this.nota3) / 3;
+    }
+
+    public String calcularStatus() {
         if (this.media >= 7) {
             this.status = "Aprovado";
         } else if (this.media <= 5) {
@@ -25,5 +75,7 @@ public class Aluno {
         } else {
             this.status = "Recuperação";
         }
+
+        return this.status;
     }
 }
